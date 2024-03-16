71246a0d311098b28d0eb4ff53b1e286c0bde6d6
Hao Duong <haoduong1141@gmail.com>
2024-03-15 23:25:28 -0700

    Day 3
    UseEffect practice

--- app/UseEffectPage/page.tsx
+++ app/UseEffectPage/page.tsx
@@ -1,7 +1,22 @@
-import React from "react";
+"use client";
+import { useState, useEffect } from "react";
 
 const UseEffectPage = () => {
-    return <div>UseEffectPage</div>;
+    const [users, setUser] = useState([]);
+
+    useEffect(() => {
+        fetch("https://jsonplaceholder.typicode.com/users")
+            .then((res) => res.json())
+            .then((data) => setUser(data));
+    }, []);
+
+    return (
+        <ul className="flex flex-col items-center justify-center pt-20 font-bold text-xl gap-4">
+            {users.map((user) => (
+                <li key={user.id}>{user.name}</li>
+            ))}
+        </ul>
+    );
 };
 
 export default UseEffectPage;
